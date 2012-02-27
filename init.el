d1773d679c55326af866d139e767dcac9f866a15
Avdi Grimm <avdi@avdi.org>
2012-02-27 18:48:51 -0500

    Initial EPUB and MOBI output.

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 (setq org-export-html-style-extra
-      (concat "<style type='text/css'>\n"
+      (concat "\n<style type='text/css'>\n"
               (with-temp-buffer
                 (insert-file-contents 
                  (or 
                   (getenv "STYLESHEET")
                   ("No $STYLESHEET set")))
                 (buffer-string))
-              "\n</style>"))
+              "\n</style>\n"))
--- orgpress.mk
+++ orgpress.mk
@@ -1,25 +1,71 @@
+ORGPRESS_VERSION	:= 0.0.1
 ORGPRESS_MAKEFILE	:= $(lastword $(MAKEFILE_LIST))
 ORGPRESS_ROOT		:= $(dir $(ORGPRESS_MAKEFILE))
 MAKEFILES		:= $(ORGPRESS_MAKEFILE)
-ORGPRESS_VERSION	:= 0.0.1
-EMACS			:= $(shell which emacs)
-EMACS_VERSION		:= $(shell $(EMACS) --version | head -n1)
-EMACS_INIT		:= $(ORGPRESS_ROOT)/init.el
-BOOK_NAME               := $(notdir $(CURDIR))
-SOURCE_FILE		:= $(BOOK_NAME).org
-BOOK_TITLE		:= $(BOOK_NAME)
+
+# Tools
+EMACS			= $(shell which emacs)
+EMACS_VERSION		= $(shell $(EMACS) --version | head -n1)
+EMACS_INIT		= $(ORGPRESS_ROOT)/init.el
+CONVERT			= ebook-convert
+define CONVERTFLAGS
+--authors "$(AUTHORS)" --book-producer "$(PRODUCER)"
+--comments "$(COMMENTS)"
+--language "$(LANGUAGE)" --publisher "$(PUBLISHER)"
+--pubdate "$(PUBDATE)" --title "$(BOOK_TITLE)"
+--use-auto-toc --no-chapters-in-toc
+--toc-threshold 0 --max-toc-links 0
+--chapter "//h:div[@class='outline-2']/h:h2"
+--level1-toc "//h:div[@class='outline-2']/h:h2" 
+--level2-toc "//h:div[@class='outline-3']/h:h3" 
+--level3-toc "//h:div[@class='outline-4']/h:h4" 
+--extra-css ".example { border: 1pt solid black; padding: 0.5ex; }"
+endef
+define EPUBFLAGS
+--output-profile ipad  --preserve-cover-aspect-ratio
+endef
+# --cover "$(EPUBCOVER)"
+define MOBIFLAGS
+--output-profile kindle 
+endef
+# --cover "$(MOBICOVER)"
+CALIBRE_INPUT		= $(call flavor_file,calibre.html)
+
+# Book metadata
+BOOK_NAME               = $(notdir $(CURDIR))
+SOURCE_FILE		= $(BOOK_NAME).org
+BOOK_TITLE		= $(BOOK_NAME)
+AUTHORS			= Unknown Author
+PRODUCER		= $(AUTHOR)
+COMMENTS		= $(BOOK_TITLE)
+LANGUAGE		= en-US
+PUBLISHER		= $(AUTHORS)
+PUBDATE			= $(shell ls -ldc $(CURDIR) | cut -d' ' -f6)
+
+# Org export customizations
 HEADLINE_LEVELS		:= 5
-LOCALE			:= en-US
+TABLE_OF_CONTENTS	:= nil
+SECTION_NUMBERS		:= nil
+
+# Misc setup
 BUILD_DIR		:= $(CURDIR)/build
-export STYLESHEET	:= $(ORGPRESS_ROOT)/styles.css
+export STYLESHEET	= $(ORGPRESS_ROOT)/styles.css
+ALL_FLAVORS		= epub mobi pdf html calibre.html
+BUNDLE_FLAVORS		= epub mobi pdf html
+CALIBRE_FLAVORS		= epub mobi
+BUNDLE_FILES		= $(BUNDLE_FLAVORS:%=$(BUILD_DIR)/$(BOOK_NAME).%)
+CALIBRE_OUTPUTS		= $(foreach flavor,$(CALIBRE_FLAVORS),$(call flavor_file,$(flavor)))
 
 export_target		:= $(BUILD_DIR)/$(BOOK_NAME).$(FLAVOR)
 
+# Functions
+flavor_file		= $(BUILD_DIR)/$(BOOK_NAME).$(1)
+
 define export_plist_calibre_elisp
-:table-of-contents	nil
+:table-of-contents	$(TABLE_OF_CONTENTS)
 :headline-levels	$(HEADLINE_LEVELS)
-:section-numbers	nil
-:language		$(LOCALE)
+:section-numbers	$(SECTION_NUMBERS)
+:language		$(LANGUAGE)
 endef
 
 define export_command_calibre_elisp
@@ -38,12 +84,16 @@ $(info OrgPress version $(ORGPRESS_VERSION))
 include $(CURDIR)/book.mk
 
 $(info Building $(BOOK_NAME))
+$(info Calibre outputs: $(CALIBRE_OUTPUTS))
 
-default: export FLAVOR=calibre.html
-default:
-	$(MAKE) selected_flavor
+default: $(BUNDLE_FLAVORS)
 
-selected_flavor: $(export_target)
+$(ALL_FLAVORS):
+	$(MAKE) $(call flavor_file,$@)
+
+$(CALIBRE_OUTPUTS): FLAVOR=$(subst .,,$(suffix $@))
+$(CALIBRE_OUTPUTS): $(CALIBRE_INPUT) $(CURDIR)/book.mk $(ORGPRESS_MAKEFILE)
+	$(CONVERT) $< $@ $(strip $(CONVERTFLAGS)) $(strip $($(FLAVOR)FLAGS))
 
 $(export_target): $(EMACS_INIT) $(SOURCE_FILE)
 	$(EMACS) $(EMACS_INIT:%=-l %) \
