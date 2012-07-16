fdab21e616bf8e16d077ac92b7af9760f202fda5
Avdi Grimm <avdi@avdi.org>
2012-07-16 00:32:54 -0400

    Only zip up assets if there are any.

--- lib/stages/package/html.mk
+++ lib/stages/package/html.mk
@@ -9,6 +9,8 @@ outputs			= $(OP_BOOK_NAME).html.zip
 %.html.zip: %.html $(LOCAL_ASSET_TARGETS) $(COMPOSITE_STYLESHEET)
 	$(ZIP) $(ZIPFLAGS) $@ -j $<
 	$(ZIP) $(ZIPFLAGS) $@ -j $(COMPOSITE_STYLESHEET)
-	$(ZIP) $(ZIPFLAGS) $@ $(ASSET_TARGETS)
+	if [ -n "$(ASSET_TARGETS)" ]; then \
+		$(ZIP) $(ZIPFLAGS) $@ $(ASSET_TARGETS); \
+	fi
 	$(ADD_FONTS_TO_ARCHIVE)
 
